f506c2dab87fe758f966eb470d360ed5a9acff5e
Trang Tran <trantrang181197@gmail.com>
2023-07-13 14:18:08 -0700

    connected to MongoDB and started server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoosef from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0'
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, HOST, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
