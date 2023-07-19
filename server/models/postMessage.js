3aa3cf11d1201fb1e744015d251075c603d10253
Trang Tran <trantrang181197@gmail.com>
2023-07-18 19:47:17 -0700

    added attributes to messages Chema

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -5,6 +5,7 @@ const postSchema = mongoose.Schema({
     message: String,
     tags: [String],
     selectedFile: String,
+    category: String,
     likeCount: {
         type: Number,
         default: 0
