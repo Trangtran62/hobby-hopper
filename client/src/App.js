0b693a719dd3b4e4387a24af2d32e6afb26713ee
Trang Tran <trantrang181197@gmail.com>
2023-07-18 19:45:32 -0700

    configured reducers in app

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import { getPosts } from './actions/posts';
+import { postsGetAll } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch]);
+    // useEffect(() => {
+    //     dispatch(postsGetAll());
+    // }, [dispatch]);
 
     return (
         <Container maxWidth='lg'> 
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+    createSlice, 
+    createAsyncThunk,
+    createEntityAdapter
+} from '@reduxjs/toolkit';
+
+import * as api from '../api';
+
+const postsAdapter = createEntityAdapter();
+
+const initialState = postsAdapter.getInitialState({status: 'idle'});
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await api.fetchPosts();
+    return response;
+});
 
 const postsSlice = createSlice({
-    name: 'apiCalls',
-    initialState: [],
+    name: 'posts',
+    initialState,
     reducers: {
-        postAdded(state, action) {
-            return state;
-        },
         postCreated(state, action) {
             return state;
         }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                postsAdapter.setAll(state, action.payload);
+                state.status = 'idle';
+            })
     }
 });
 
-export const { postAdded, postCreated } = postsSlice.actions;
+export const { postCreated } = postsSlice.actions;
 export default postsSlice.reducer;
