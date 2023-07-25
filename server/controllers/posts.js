e93cb221ebc8ae526c2c6fb03d9f7c16ff3f8a79
Trang Tran <trantrang181197@gmail.com>
2023-07-24 17:39:35 -0700

    added likeCount increment patch route for backend API

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -44,3 +44,16 @@ export const deletePost = async (req, res) => {
 
     res.json({ message: 'Post successfully deleted' });
 }
+
+export const likePost = async (req, res) => {
+    const {id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No id found');
+    }
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
+}
