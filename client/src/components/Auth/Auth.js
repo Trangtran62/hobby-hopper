9ed075957bd00b6d17964e3a90e7bf8ff96ed7af
Trang Tran <trantrang181197@gmail.com>
2023-07-25 12:03:07 -0700

    setup skeleton for authentication page

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import useStyles from './styles';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import Input from './Input';
 
 const Auth = () => {
-    return (
-        <div>
+    const classes = useStyles();
+    const isSignup = false;
+    const [showPassword, setShowPassword] = useState(false);
 
-        </div>
+    const handleSubmit = () => {};
+    const handleChange = () => {};
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LoginOutlinedIcon />
+                </Avatar>
+                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        { isSignup && (
+                                <>
+                                    <Input 
+                                        name="firstName" 
+                                        label="First Name" 
+                                        handleChange={handleChange}
+                                        autoFocus
+                                        half
+                                    />
+                                    <Input 
+                                        name="firstName" 
+                                        label="First Name" 
+                                        handleChange={handleChange}
+                                        half
+                                    />                             
+                                </>
+                        )}
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type="ps" />
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 };
 
---
+++ client/src/components/Auth/styles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+        marginTop: theme.spacing(8),
+    },
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+    },
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: deepPurple[300]
+    },
+    form: {
+        marginTop: theme.spacing(3),
+        width: '100%', // Fix IE 11 issue.
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    googleButton: {
+        marginBottom: theme.spacing(2),
+    },
+}));
