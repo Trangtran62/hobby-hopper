2c2e5862bdc30a2f4248e216167312b5d46e2e0d
Trang Tran <trantrang181197@gmail.com>
2023-07-26 19:49:27 -0700

    created database for users

--- server/index.js
+++ server/index.js
@@ -7,15 +7,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
+
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0'
 
