2588066dd8d8f7731df8ac21ad8c93f9ac469a3a
Trang Tran <trantrang181197@gmail.com>
2023-07-26 21:01:54 -0700

    created auth middelware for controlling user interaction with posts

---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500; // Google token is > 500
+
+        let decodedData;
+
+        if (token && isCustomAuth) {
+            decodedData = jwt.verify(token, process.env.TOKEN);
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token); // Google token
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export default auth;
