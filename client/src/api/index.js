9d177eefa69adbc7c61619f7dccf458fba2b2cc9
Trang Tran <trantrang181197@gmail.com>
2023-08-17 19:56:37 +0700

    changed api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080' })
+const API = axios.create({ baseURL: 'https://hobby-hopper-api.onrender.com' })
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
         const token = JSON.parse(localStorage.getItem('profile')).token;
